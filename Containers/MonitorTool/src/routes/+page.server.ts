f5c868594a07e003b34aa347d82c3e88da70ccc2
Savvas Mantzouranidis <s.mantzouranidis@bristol.ac.uk>
2024-12-20 12:47:12 +0000

    format status updates

--- Containers/MonitorTool/src/routes/+page.server.ts
+++ Containers/MonitorTool/src/routes/+page.server.ts
@@ -2,9 +2,8 @@ import type { PageServerLoad } from './$types';
 import { getCollection } from '$lib/db/collections';
 
 export const load = (async () => {
-	// get repositories from MongoDB
+	// get statusUpdates from MongoDB
 	const statusUpdates = await getCollection('statusUpdates', 0, 10);
 
-	console.log(statusUpdates);
 	return { data: statusUpdates };
 }) satisfies PageServerLoad;
--- Containers/MonitorTool/src/routes/+page.svelte
+++ Containers/MonitorTool/src/routes/+page.svelte
@@ -2,13 +2,17 @@
 	const { data } = $props();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-{#if Array.isArray(data.data)}
-	{#each data.data as update}
-		<pre>{JSON.stringify(update, null, 2)}</pre>
-	{/each}
-{:else}
-	<p>No updates available</p>
-{/if}
+<div>
+	<h2 class="text-xl font-semibold">Logs</h2>
+	<div class="flex flex-col rounded-lg border p-4">
+		{#if Array.isArray(data.data)}
+			{#each data.data as update}
+				<span>
+					{new Date(update.timestamp).toISOString().slice(0, 19).replace('T', ' ')}: {update.message}
+				</span>
+			{/each}
+		{:else}
+			<p>No updates available</p>
+		{/if}
+	</div>
+</div>
