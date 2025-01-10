feab04a03dcbdcffaa6e325dda1136ad98bc4d48
Savvas Mantzouranidis <s.mantzouranidis@bristol.ac.uk>
2025-01-10 11:02:34 +0000

    initial line chart

--- Containers/MonitorTool/src/routes/+page.svelte
+++ Containers/MonitorTool/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import type { ProcessingMetrics } from '$lib/db/collections';
 	import { onDestroy, onMount } from 'svelte';
+	import { Chart, Svg, Axis, Spline, Text, Tooltip, Highlight } from 'layerchart';
+	import { scaleTime, scaleOrdinal } from 'd3-scale';
+	import { formatDate, PeriodType } from '@layerstack/utils';
+	import { format } from 'date-fns';
+	import { flatGroup } from 'd3-array';
 
 	type StatusUpdate = {
 		timestamp: string;
@@ -17,9 +22,27 @@
 		stats: { [key: string]: { count: number; metrics: ProcessingMetrics } };
 	};
 
+	const keyColors = {
+		files: 'red',
+		chunks: 'green',
+		candidates: 'blue',
+		clones: 'purple'
+	};
+
 	let statusUpdates: StatusUpdate[] = $state([]);
 	let collectionStats: Map<string, CollectionStats> = $state(new Map());
 	let allStatistics: AllStatistics[] = $state([]);
+	let allStatisticsFlat: {
+		timestamp: number;
+		key: string;
+		count: number;
+		itemsPerSecond: number;
+		itemProcessingTime: number;
+	}[] = $state([]);
+	let dataByKey: Map<
+		string,
+		{ timestamp: number; count: number; itemsPerSecond: number; itemProcessingTime: number }[]
+	> = $state(new Map());
 
 	async function fetchData() {
 		fetch('/api/status')
@@ -31,6 +54,20 @@
 			.then((res) => res.json())
 			.then((data) => {
 				allStatistics = data.allStatistics;
+				allStatisticsFlat = allStatistics.flatMap((stat) => {
+					return Object.entries(stat.stats).map(([key, value]) => {
+						return {
+							timestamp: stat.timestamp,
+							key,
+							count: value.count,
+							itemsPerSecond: value.metrics.processingRate,
+							itemProcessingTime: value.metrics.timePerDocument
+						};
+					});
+				});
+				dataByKey = flatGroup(allStatisticsFlat, (d) => d.key);
+				console.log(dataByKey);
+
 				collectionStats = data.allStatistics[0].stats;
 			});
 
@@ -50,6 +87,50 @@
 		interval && clearInterval(interval);
 	});
 </script>
+
+<div class="h-[300px] rounded border p-4">
+	<Chart
+		data={allStatisticsFlat}
+		x="timestamp"
+		xScale={scaleTime()}
+		y="itemsPerSecond"
+		yDomain={[0, null]}
+		yNice
+		c="key"
+		cScale={scaleOrdinal()}
+		cDomain={Object.keys(keyColors)}
+		cRange={Object.values(keyColors)}
+		padding={{ left: 16, bottom: 24, right: 48 }}
+		tooltip={{ mode: 'voronoi' }}
+		let:cScale
+	>
+		<Svg>
+			<Axis placement="left" grid rule />
+			<Axis
+				placement="bottom"
+				format={(d) => formatDate(d, PeriodType.Day, { variant: 'short' })}
+				rule
+			/>
+			{#each dataByKey as [key, data]}
+				{@const color = cScale?.(key)}
+				<Spline {data} class="stroke-2" stroke={color}>
+					<svelte:fragment slot="end">
+						<circle r={4} fill={color} />
+						<Text value={key} verticalAnchor="middle" dx={6} dy={-2} class="text-xs" fill={color} />
+					</svelte:fragment>
+				</Spline>
+			{/each}
+			<Highlight points lines />
+		</Svg>
+
+		<Tooltip.Root let:data>
+			<Tooltip.Header>{format(data.date, 'eee, MMMM do')}</Tooltip.Header>
+			<Tooltip.List>
+				<Tooltip.Item label={data.fruit} value={data.value} />
+			</Tooltip.List>
+		</Tooltip.Root>
+	</Chart>
+</div>
 
 <div class="grid w-full grid-cols-5 gap-2">
 	<div class="col-span-3 flex flex-1 flex-col gap-1">
